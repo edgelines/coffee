f35864d1f7e737b7d134842ca80d46eca3f93518
DESKTOP-K2STHML\Sang Jin <mitotero@kakao.com>
2023-12-13 23:04:34 +0900

    경로변경

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     mode: "production",
     devtool: "hidden-source-map",
     output: {
-        path: path.resolve('D:/web/coffee'),
+        path: path.resolve('D:/CheckMate/FastAPI/web/coffee'),
         filename: 'build.js',
         publicPath: '/', // 라우터 링크 못읽는 현상 수정
     },
@@ -17,7 +17,7 @@ module.exports = merge(common, {
             template: path.resolve(__dirname, 'src', 'index.html'),  // index.html 파일의 경로를 지정합니다.
             favicon: path.resolve(__dirname, 'src', 'favicon.ico'),
             inject: true,
-            filename: path.resolve('D:/web/coffee/index.html')
+            filename: path.resolve('D:/CheckMate/FastAPI/web/coffee/index.html')
         }),
     ],
 });
